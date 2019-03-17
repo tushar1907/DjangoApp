b25f05d4d2410ed4ae87bbd68a3870bb8f6d84e9
Tushar <gupta.tus@husky.neu.edu>
2019-03-17 17:32:16 -0400

    other urls's created

--- mySite/polls/urls.py
+++ mySite/polls/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns =[
 
     url(r'^$', views.index, name='index'),
+
+    url(r'^/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    url(r'^/(?P<question_id>[0-9]+)/result$', views.result, name='result'),
+
+    url(r'^/(?P<question_id>[0-9]+)/vote$', views.vote, name='vote'),
+
+
 ]
--- mySite/polls/views.py
+++ mySite/polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Awesome job guys")
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view: %s" %question_id);
+
+
+def result(request, question_id):
+    return HttpResponse("This is the result view: %s" %question_id);
+
+
+def vote(request, question_id):
+    return HttpResponse("This is the vote view: %s" %question_id);
