caf2706eccd5a0b51541c0e0d0aad280a1c72eba
Tushar <gupta.tus@husky.neu.edu>
2019-03-18 21:46:52 -0400

    Polls app include ans other tags

--- mySite/polls/models.py
+++ mySite/polls/models.py
@@ -7,14 +7,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date_published')
 
-    def _str__(self):
+    def __str__(self):
         return self.question_text
 
 
 class Choice(models.Model):
-    question_text = models.CharField(max_length=100)
+    choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default = 0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
-    def _str__(self):
+    def __str__(self):
         return self.choice_text
--- mySite/polls/views.py
+++ mySite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
@@ -11,7 +11,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("This is the detail view: %s" %question_id)
+    question = get_object_or_404(Question,pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def result(request, question_id):
